56087549aea260c8c473227e881a988d8258cf8d
Robert <robethuku11@gmail.com>
2023-02-28 07:29:54 +0300

    Completed practise

---
+++ 01_insert_cats_into_cats_table.sql
@@ -0,0 +1,4 @@
+INSERT INTO cats (name,age,breed) VALUES ("lil Bub",5,"American Shorthair");
+INSERT INTO cats (name,age,breed) VALUES ("Hannah",1,"TAbby");
+INSERT INTO cats (name,age,breed) VALUES ("moe",10,"Tabby");
+INSERT INTO cats (name,age,breed) VALUES ("patches",2,"Calico");
